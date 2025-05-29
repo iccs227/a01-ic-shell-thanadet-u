70c116ccb5e8f8d3a03575918235048df117349d
tu.perth <74957058+thanadet-u@users.noreply.github.com>
2025-05-29 21:33:58 +0700

    fix: builtins, parser

--- src/builtins.cpp
+++ src/builtins.cpp
@@ -1,43 +1,51 @@
 #include <iostream>
+#include <stdexcept>
 #include <string>
+#include <vector>
+#include "header/builtins.h"
+#include "header/icsh.h"
 using namespace std;
 
 int builtin(vector<string> args) {
 
-	int argc = args.size();
+  int argc = args.size();
 
-	if (argc == 0) {
-		return 0;
-	}
-	string command = args[0];
+  if (argc == 0) {
+    return 0;
+  }
+  string command = args[0];
 
-	// TODO: get exit code then exit
-	if (command == "exit") {
-		if (argc == 2) {
-			const int exit_value = stol(args[1]) & 0xFF;
-			cout << "adios" << endl;
-			exit(exit_value);
-		}
-		cout << "Invalid exitcode" << endl;
-		cout << "bad command" << endl;
-		return 0;
+  if (command == "exit") {
+    if (argc == 2) {
+      try {
+        const int exit_value = stol(args[1]) & 0xFF;
+        cout << "adios" << endl;
+        exit(exit_value);
+      } catch (invalid_argument e) {
+        exit(-1);
+      }
+    }
+    cout << "ERROR: Invalid exitcode" << endl;
+    cout << "bad command" << endl;
+    return 0;
+  }
 
-	}
+  if (command == "echo") {
+    if (argc <= 1) {
+    }
+    if (argc == 2 && args[1] == "$?") {
+      cout << exit_status_code << "\n";
+    }
+    else {
+      for (int i = 1; i < argc; i++) {
+        cout << args[i] << " ";
+      }
+      cout << ("\n");
+    }
+    return 0;
+  }
 
-	if (command == "echo") {
-		if (argc <= 1) {
-		}
-		else {
-			for (int i = 1; i < argc ; i++) {
-				cout << args[i] << " ";
-			}
-			cout << ("\n");
-		}
-		return 0;
-	}
-
-	// cout << "bad command" << endl;	//
-	// Not a built-in command return -1
-	return -1;
-
+  // cout << "bad command" << endl;	//
+  // Not a built-in command return -1
+  return -1;
 }
--- src/parser.cpp
+++ src/parser.cpp
@@ -2,11 +2,12 @@
 #include <sstream>
 #include <string>
 #include "header/parser.h"
+using namespace std;
 
-std::vector<std::string> input_parser(const std::string& input) {
-	std::vector<std::string> tokens;
-	std::string token;
-	std::istringstream tokenStream(input);
+vector<string> input_parser(const string& input) {
+	vector<std::string> tokens;
+	string token;
+	istringstream tokenStream(input);
 	while (std::getline(tokenStream, token, ' ')) {
 		if (!token.empty()) {
 			tokens.push_back(token);
